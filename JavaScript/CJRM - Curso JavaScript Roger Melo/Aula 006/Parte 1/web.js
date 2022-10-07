2e3a48b8fa793e75b280735b64a894e0af9dae11
Leosonicx <leonardocarmo941@gmail.com>
2022-10-07 18:54:44 -0300

    Aprendendo if e else

---
+++ JavaScript/CJRM - Curso JavaScript Roger Melo/Aula 006/Parte 1/web.js
@@ -0,0 +1,8 @@
+// if e else if statements
+const password = 'oi123'
+
+if (password.length >= 8) {
+  console.log('Essa senha tem 8 ou mais caracteres') // Caso isso seja TRUE, essa mensagem irá aparecer.
+} else {
+  console.log('A senha deve conter 8 ou mais caracteres') // Caso o password seja FALSE, essa mensagem aparece. 
+}
