a2de07672292ac1c30a6350803f0a1cbee696b27
Leosonicx <leonardocarmo941@gmail.com>
2022-10-14 13:42:05 -0300

    Aprendendo sobre Objetos

---
+++ JavaScript/CJRM - Curso JavaScript Roger Melo/Aula 010/Parte 1/web.js
@@ -0,0 +1,10 @@
+// Criando um objeto literal
+let user = {
+  name: 'Leonardo', 
+  age: 20,
+  email: 'leonardocarmo941@gmail.com',
+  city: 'Itajubá',
+  curiositys: ['Amante de jogos', 'Programador', 'Amante de mangás']
+}
+
+console.log(typeof user) // Ao fazer isso, podemos ver que user é um objeto.
