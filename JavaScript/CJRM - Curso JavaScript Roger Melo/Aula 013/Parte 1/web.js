718e31a4b726f62bb128b8ed55c2761d99273ea0
Leosonicx <leonardocarmo941@gmail.com>
2022-10-18 17:25:09 -0200

    Aprendendo mais sobre escopos

--- JavaScript/CJRM - Curso JavaScript Roger Melo/Aula 013/Parte 1/web.js
+++ JavaScript/CJRM - Curso JavaScript Roger Melo/Aula 013/Parte 1/web.js
@@ -1,1 +1,49 @@
-//
+// function myFunc () {
+//   let cat = 'zeca'
+//   const age = 3
+//   var color = 'cinza'
+//   console.log(age) // Irá mostrar apenas o age DESTE bloco.
+// }
+
+// function myFunc2 () {
+//   const age = 5
+//   var color = 'branco'
+//   console.log(age) // Irá mostrar apenas o age DESTE bloco.
+// }
+
+// myFunc()
+// myFunc2()
+
+// const dog = 'Raça-mista'
+
+// function dogWatch () {
+//   const dog = 'Simba'
+//   console.log(dog)
+// }
+
+// dogWatch()
+// console.log(dog)
+
+// if (true) {
+//   var dragon = 'Balerion'
+//   console.log(dragon) 
+// }
+
+// console.log(dragon) // Irá mostrar dois consoles, pois a var não especifica um escopo.
+
+const external = () => {
+  const book = 'Sapiens'
+
+  const internal = () => {
+    const book = 'Harry Potter'
+    console.log(book.toUpperCase())
+  }
+
+    const extraInteral = () => {
+      console.log(book.toLocaleUpperCase) // Irá achar a variavel da interal, pois é o escopo mais bloco.
+    }
+
+  internal() 
+}
+
+external()
