92cc61930769a20dbf7591a1c2d9af75bd598e12
Leosonicx <leonardocarmo941@gmail.com>
2022-10-18 16:20:37 -0200

    Exercício 012 - CJRM

--- JavaScript/CJRM - Curso JavaScript Roger Melo/Exercicios CJRM/exercicio-12/app.js
+++ JavaScript/CJRM - Curso JavaScript Roger Melo/Exercicios CJRM/exercicio-12/app.js
@@ -27,6 +27,17 @@ const best2019Movies = [
   { title: 'A Vida Invisível', directedBy: 'Karim Aïnouz' }
 ]
 
+let message = 'Segundo o site Omelete, os melhores filmes de 2019 são:'
+
+const generateMovieMessage = movie => {
+  message += `
+  - ${movie.title}, dirigido por ${movie.title}`
+}
+
+best2019Movies.forEach(generateMovieMessage)
+
+console.log(message)
+
 /*
   02
 
@@ -59,8 +70,17 @@ const youtubeUser = {
       gitHub: 'https://github.com/Roger-Melo'
     },
     country: 'Brasil'
+  },
+  getRecenteVideos () {
+    console.log(`Vídeos recentes de ${this.name}:`)
+
+    this.videos.recentVideos.forEach(video => {
+      console.log(video.title)
+    })
   }
 }
+
+youtubeUser.getRecenteVideos()
 
 /*
   03
@@ -68,7 +88,7 @@ const youtubeUser = {
   - Exiba o valor do PI no console.
 */
 
-
+console.log(Math.PI)
 
 /*
   04
@@ -79,6 +99,8 @@ const youtubeUser = {
 
 const firstNumber = 8.3
 
+console.log(Math.ceil(firstNumber))
+
 /*
   05
 
@@ -87,6 +109,8 @@ const firstNumber = 8.3
 */
 
 const secondNumber = 3.5
+
+console.log(Math.round(secondNumber))
 
 /*
   06
@@ -97,6 +121,8 @@ const secondNumber = 3.5
 
 const thirdNumber = 8.9
 
+console.log(Math.floor(thirdNumber))
+
 /*
   07
 
@@ -105,9 +131,15 @@ const thirdNumber = 8.9
 
 const fourthNumber = 5.5
 
+console.log(Math.trunc(fourthNumber))
+
 /*
   08
 
   - A cada vez que o index.html for carregado, exiba no console um número  
     aleatório de 0 à 10, incluindo 0 e 10.
 */
+
+const randolarNumero = Math.random()
+
+console.log(Math.round(randolarNumero * 10))
